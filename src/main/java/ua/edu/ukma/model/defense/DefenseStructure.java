6f4cb4e780615383fae88dfe4ff08bd7166ac066
Daria Trukhan <dashamegavip@gmail.com>
2026-06-01 18:02:47 +0300

    feat: add ImageView to DefenseStructure and load textures

--- src/main/java/ua/edu/ukma/model/defense/DefenseStructure.java
+++ src/main/java/ua/edu/ukma/model/defense/DefenseStructure.java
@@ -1,16 +1,34 @@
 package ua.edu.ukma.model.defense;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.util.Objects;
+
 public class DefenseStructure {
     private final int row;
     private final int col;
     private final DefenseType type;
+    private final ImageView imageView;
 
     public DefenseStructure(int row, int col, DefenseType type) {
         this.row = row;
         this.col = col;
         this.type = type;
-    }
 
+        try {
+            String texturePath = type.texturePath();
+            Image sprite = new Image(Objects.requireNonNull(getClass().getResourceAsStream(texturePath)));
+
+            this.imageView = new ImageView(sprite);
+            this.imageView.setFitWidth(32);
+            this.imageView.setFitHeight(32);
+            this.imageView.setPreserveRatio(true);
+            this.imageView.setMouseTransparent(true);
+            updateImageViewPosition(48);
+        } catch (Exception e) {
+            throw new IllegalStateException( "Cannot read texture for: " + type.name(), e);
+        }}
     public int getRow() {
         return row;
     }
@@ -21,5 +39,17 @@ public class DefenseStructure {
 
     public DefenseType getType() {
         return type;
+    }
+    public ImageView getView() {
+        return imageView;
+    }
+    public void updateImageViewPosition(int tileSize) {
+        if (imageView == null) return;
+
+        double pixelX = col * tileSize + (tileSize - imageView.getFitWidth()) / 2.0;
+        double pixelY = row * tileSize + (tileSize - imageView.getFitHeight()) / 2.0;
+
+        this.imageView.setLayoutX(pixelX);
+        this.imageView.setLayoutY(pixelY);
     }
 }
